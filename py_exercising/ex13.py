15eb583e54b49725df81aea3affda7df98f30d60
DaeHyun <g24481862@gmail.com>
2020-07-15 18:18:21 +0900

    Add python examples 8~13

---
+++ py_exercising/ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+스크립트, 첫번째, 두번째, 세번째 = argv
+
+print("스크립트 이름 : ",스크립트)
+print("첫번째 변수 : ",첫번째)
+print("두번째 변수 : ",두번째)
+print("세번째 변수 : ",세번째)
---
+++ py_exercising/ex8.py
@@ -0,0 +1,11 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format("하나", "둘", "셋", "넷"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(
+	"난 이게 있죠.",
+	"지금 막 써주신 그것.",
+	"하지만 '노래'하진 않아요.",
+	"그러니까 잘 자요."
+))
